e2293c112034f88198064dd25abc55e41803e767
ruthnot <sean.ruthnot@gmail.com>
2023-03-22 23:08:37 -0700

    add keywords

--- add_keyword.py
+++ add_keyword.py
@@ -6,19 +6,21 @@ def main():
     today = db.today
     keywords = db.keywords
     while True:
-        print('Please type your keyword and type:')
+        print('Please type your keyword')
         user_input = input()
         if user_input == 'exit':
             print('Have a nice day!')
             break
-        keyword, type, *others = user_input.split(' ')
+        # keyword, type, *others = user_input.split(' ')
+        keyword = user_input
         if keyword in keywords:
             print('Keyword already existed!')
             continue
-        keywords[keyword] = {'date_added': today, 'review_history': [[today, 'h']], 'type': type, 'priority': 100.}
+        keywords[keyword] = {'date_added': today, 'review_history': [[today, 'h']], 'type': None, 'priority': 100.}
 
-        print(f'Added keyword: {keyword} type: {type}!\n')
+        print(f'Added keyword: {keyword} !\n')
     db.overwrite(keywords)
+
 
 if __name__=='__main__':
     main()
