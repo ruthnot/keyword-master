b351a8960a15c54b315a6b2cef7c313446d7221c
ruthnot <sean.ruthnot@gmail.com>
2023-12-24 10:27:10 -0800

    update

--- quiz.py
+++ quiz.py
@@ -2,14 +2,14 @@ import random
 from db_helper import DBHelper
 from compute_priority import ComputePriority
 
-TOPIC = "voc"
-# TOPIC = "ALL"
+# TOPIC = "voc"
+TOPIC = "ALL"
 
 SHUFFLE = False
 # SHUFFLE = True
 
-ONLY_NEW = False
-# ONLY_NEW = True
+# ONLY_NEW = False
+ONLY_NEW = True
 #
 
 def main():
